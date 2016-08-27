dd8418c724ba37a7131abf7f2551102d5787ca4a
DenizUgur <deniz343@gmail.com>
2016-08-27 20:28:53 +0300

    update

--- gallery/assets/js/main.js
+++ gallery/assets/js/main.js
@@ -5,6 +5,34 @@
 */
 
 var main = (function ($) {
+    
+    function tooltip() {
+        $.ajax({
+            type: "GET",
+            url: "./images/images.xml",
+            dataType: "xml",
+            success: xmlParser
+        });
+
+        function xmlParser(xml) {
+
+            var id = $(".active > a").first().attr("id");
+
+            var $xml = $(xml),
+                size = $xml.find("images[id='" + id + "'] size").text(),
+                dh = $xml.find("images[id='" + id + "'] dh").text(),
+                dv = $xml.find("images[id='" + id + "'] dv").text();
+
+            var str = size + ", " + dh + "x" + dv;
+
+            $("p > a").first()
+                .attr("data-toggle", "tooltip")
+                .attr("title", str);
+            $('[data-toggle="tooltip"]').tooltip();
+
+        }
+    }
+    
     var _ = {
 
         /**
@@ -632,7 +660,7 @@ var main = (function ($) {
             // Otherwise, wait for old slide to disappear first.
             else
                 window.setTimeout(f, _.settings.slideDuration);
-
+            
             tooltip();
 
         },
