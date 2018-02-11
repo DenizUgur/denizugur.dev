53ca83e6a5147b9e4637c4d09ec15021478d894c
DenizUgur <deniz343@gmail.com>
2018-02-11 13:06:05 +0300

    ref final mod

--- assets/js/main.js
+++ assets/js/main.js
@@ -2,11 +2,11 @@ $(document).ready(function () {
     var data = {
         "google": "https://www.google.com"
     };
-    
+
     var hash = window.location.hash.substring(1);
 
     $.each(data, function (key, val) {
-        if (hash.equals(key)) {
+        if (hash === key) {
             window.location = val;
         }
     });
