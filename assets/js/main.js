fb10f792d5236a5d7b67783a0b5280e91493fafb
DenizUgur <deniz343@gmail.com>
2018-02-11 12:55:15 +0300

    $

--- assets/js/main.js
+++ assets/js/main.js
@@ -2,7 +2,7 @@
 (function () {
     var hash = window.location.hash.substring(1);
 
-    $.getJSON("refs.json", function (data) {
+    $.get("refs.json", function (data) {
         $.each(data, function (key, val) {
             if (hash.equals(key)) {
                 window.location = val;
