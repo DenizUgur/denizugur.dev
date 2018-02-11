30f9d872e5cedecb5d92f0dce94c6fbd414142c2
Deniz Uğur <DenizUgur@users.noreply.github.com>
2018-02-11 14:43:36 +0300

    Update main.js

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,6 +1,7 @@
 $(document).ready(function () {
     var data = {
         "google": "https://www.google.com"
+        "pamporovo": "https://www.youtube.com/watch?v=E6RPadYqsKE"
     };
 
     var hash = window.location.hash.substring(1);
