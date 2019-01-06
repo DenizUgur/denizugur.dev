c8fb00b038b5d035da468d881f209aeadcc509c9
DenizUgur <deniz343@gmail.com>
2019-01-06 21:30:59 +0300

    Playbox

---
+++ assets/js/playbox.js
@@ -0,0 +1,27 @@
+$(document).ready(function () {
+    var url = "https://ws.audioscrobbler.com/2.0/?method=user.getrecenttracks&user=Deniz343&api_key=fa142978a84c511403a710029688c023&format=json&limit=1";
+
+    $.get(url, function (data) {
+        var name = data.recenttracks.track[0].name;
+        var artist = data.recenttracks.track[0].artist["#text"];
+        var album = data.recenttracks.track[0].album["#text"];
+        var image = data.recenttracks.track[0].image[2]["#text"];
+        var lastfm_url = data.recenttracks.track[0].url;
+        var nowplaying = "false";
+        if (data.recenttracks.track[0]["@attr"])
+            var nowplaying = data.recenttracks.track[0]["@attr"].nowplaying;
+
+        if (name.length > 0) {
+            $('.artist').html(artist);
+            $('.song').html(name);
+            $('.album').html(album);
+            $('.art').css('background-image', 'url("' + image + '")');
+            $(".art").on("click", function () {
+                window.open(lastfm_url, '_blank');
+            });
+        }
+        if (nowplaying == "true") {
+            $("#playbox").addClass("pulse");
+        }
+    });
+});
