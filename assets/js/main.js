b4cbdb58241719a91a59d8bfdf4ad4249e17d7c6
DenizUgur <deniz343@gmail.com>
2019-01-14 21:18:28 +0300

    revert-test

--- assets/js/main.js
+++ assets/js/main.js
@@ -14,43 +14,6 @@ $(document).ready(function () {
         $(".glitch-wrapper").find("h1").hide();
         $(".glitch-wrapper").find("div").show();
     }
-
-    if (hash == "test1") {
-        window.scrollTo(0, 1);
-    } else if (hash == "test2") {
-        window.scrollTo(0, 100);
-    } else {
-         var doc = window.document;
-         // If there's a hash, or addEventListener is undefined, stop here
-         if (!location.hash && window.addEventListener) {
-
-             //scroll to 1
-             window.scrollTo(0, 1);
-             var scrollTop = 1,
-                 getScrollTop = function () {
-                     return window.pageYOffset || doc.compatMode === "CSS1Compat" && doc.documentElement.scrollTop || doc.body.scrollTop || 0;
-                 },
-
-                 //reset to 0 on bodyready, if needed
-                 bodycheck = setInterval(function () {
-                     if (doc.body) {
-                         clearInterval(bodycheck);
-                         scrollTop = getScrollTop();
-                         window.scrollTo(0, scrollTop === 1 ? 0 : 1);
-                     }
-                 }, 15);
-
-             window.addEventListener("load", function () {
-                 setTimeout(function () {
-                     //at load, if user hasn't scrolled more than 20 or so...
-                     if (getScrollTop() < 20) {
-                         //reset to hide addr bar at onload
-                         window.scrollTo(0, scrollTop === 1 ? 0 : 1);
-                     }
-                 }, 0);
-             }, false);
-         }
-    }
 });
 
 function url_query(query) {
