7f51b6ccdd03f07645e213eedbf3f78b5623c3c6
DenizUgur <deniz343@gmail.com>
2020-08-06 19:35:04 +0300

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.5bb03e0095bbcd5312b7749ee26da5c3.js"
+  "/precache-manifest.68cd80d47368edb7d5ed01bfd7dd8325.js"
 );
 
 self.addEventListener('message', (event) => {
