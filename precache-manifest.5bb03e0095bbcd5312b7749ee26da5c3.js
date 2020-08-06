b662bbbd00691f02eca2c1fe9f83ee1174ed3d0b
DenizUgur <deniz343@gmail.com>
2020-08-06 18:58:31 +0300

    Updates

---
+++ precache-manifest.5bb03e0095bbcd5312b7749ee26da5c3.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b48656cfd42c85b9beead7f51b34b905",
+    "url": "/index.html"
+  },
+  {
+    "revision": "1849a8382deacbe5b0e9",
+    "url": "/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "26aa706a9e3f15836031",
+    "url": "/static/js/2.527e31df.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.527e31df.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1849a8382deacbe5b0e9",
+    "url": "/static/js/main.7f9f788d.chunk.js"
+  },
+  {
+    "revision": "b6f8986f68a810051015",
+    "url": "/static/js/runtime-main.ac15fc54.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
